d2ee6afddcb025924b54d3a31ec7db561e6e735d
K3PAA <jkepa00@gmail.com>
2022-11-11 10:44:27 +0100

    Fixing movement of Player
    Fixing movement

--- Code/app.js
+++ Code/app.js
@@ -170,6 +170,12 @@ function animate() {
     clearInterval(createBombs)
     removeEventListener('keydown', move)
   }
+
+  if (player.velocity.x > 0) {
+    player.spacing = 40
+  } else {
+    player.spacing = 8
+  }
   player.update()
 }
 
@@ -187,29 +193,7 @@ let boomed = false
 
 addEventListener('keydown', move)
 
-addEventListener('keyup', (e) => {
-  switch (e.key) {
-    case 'w':
-      clearInterval(press.up)
-      press.up = undefined
-      player.velocity.y = 0
-      break
-    case 's':
-      clearInterval(press.down)
-      press.down = undefined
-      player.velocity.y = 0
-      break
-    case 'a':
-      clearInterval(press.left)
-      press.left = undefined
-      player.velocity.x = 0
-    case 'd':
-      clearInterval(press.right)
-      press.right = undefined
-      player.velocity.x = 0
-      break
-  }
-})
+addEventListener('keyup', stop)
 
 let createBombs = undefined
 
@@ -252,106 +236,127 @@ bg.onload = () => {
 }
 
 function move(e) {
-  switch (e.key) {
-    case 'w':
-      if (!press.up) {
-        press.up = setInterval(() => {
-          for (let i = 0; i < boundaries.length; i++) {
-            const boundary = boundaries[i]
-            if (
-              rectangularCollision({
-                rectangle1: player,
-                rectangle2: {
-                  ...boundary,
-                  position: {
-                    x: boundary.position.x,
-                    y: boundary.position.y + 5,
-                  },
+  if (e.key == 'd') {
+    if (!press.right) {
+      player.spacing = 40
+      press.right = setInterval(() => {
+        for (let i = 0; i < boundaries.length; i++) {
+          const boundary = boundaries[i]
+          if (
+            rectangularCollision({
+              rectangle1: player,
+              rectangle2: {
+                ...boundary,
+                position: {
+                  x: boundary.position.x - 5,
+                  y: boundary.position.y,
                 },
-              })
-            ) {
-              player.velocity.y = 0
-              break
-            } else player.velocity.y = -movementSpeed
-          }
-        }, 1)
-      }
-      break
-    case 's':
-      if (!press.down) {
-        press.down = setInterval(() => {
-          if (press.down) {
-            for (let i = 0; i < boundaries.length; i++) {
-              const boundary = boundaries[i]
-              if (
-                rectangularCollision({
-                  rectangle1: player,
-                  rectangle2: {
-                    ...boundary,
-                    position: {
-                      x: boundary.position.x,
-                      y: boundary.position.y - 5,
-                    },
-                  },
-                })
-              ) {
-                player.velocity.y = 0
-                break
-              } else player.velocity.y = movementSpeed
-            }
-          }
-        }, 1)
-      }
-      break
-    case 'a':
-      if (!press.left) {
-        player.spacing = 8
-        press.left = setInterval(() => {
-          for (let i = 0; i < boundaries.length; i++) {
-            const boundary = boundaries[i]
-            if (
-              rectangularCollision({
-                rectangle1: player,
-                rectangle2: {
-                  ...boundary,
-                  position: {
-                    x: boundary.position.x + 5,
-                    y: boundary.position.y,
-                  },
+              },
+            })
+          ) {
+            player.velocity.x = 0
+            break
+          } else player.velocity.x = movementSpeed
+        }
+      }, 1)
+    }
+  }
+  if (e.key == 'w') {
+    if (!press.up) {
+      press.up = setInterval(() => {
+        for (let i = 0; i < boundaries.length; i++) {
+          const boundary = boundaries[i]
+          if (
+            rectangularCollision({
+              rectangle1: player,
+              rectangle2: {
+                ...boundary,
+                position: {
+                  x: boundary.position.x,
+                  y: boundary.position.y + 5,
                 },
-              })
-            ) {
-              player.velocity.x = 0
-              break
-            } else player.velocity.x = -movementSpeed
-          }
-        }, 1)
-      }
-      break
-    case 'd':
-      if (!press.right) {
-        player.spacing = 40
-        press.right = setInterval(() => {
-          for (let i = 0; i < boundaries.length; i++) {
-            const boundary = boundaries[i]
-            if (
-              rectangularCollision({
-                rectangle1: player,
-                rectangle2: {
-                  ...boundary,
-                  position: {
-                    x: boundary.position.x - 5,
-                    y: boundary.position.y,
-                  },
+              },
+            })
+          ) {
+            player.velocity.y = 0
+            break
+          } else player.velocity.y = -movementSpeed
+        }
+      }, 1)
+    }
+  }
+
+  if (e.key == 's') {
+    if (!press.down) {
+      press.down = setInterval(() => {
+        for (let i = 0; i < boundaries.length; i++) {
+          const boundary = boundaries[i]
+          if (
+            rectangularCollision({
+              rectangle1: player,
+              rectangle2: {
+                ...boundary,
+                position: {
+                  x: boundary.position.x,
+                  y: boundary.position.y - 5,
                 },
-              })
-            ) {
-              player.velocity.x = 0
-              break
-            } else player.velocity.x = movementSpeed
-          }
-        }, 1)
-      }
-      break
+              },
+            })
+          ) {
+            player.velocity.y = 0
+            break
+          } else player.velocity.y = movementSpeed
+        }
+      }, 1)
+    }
+  }
+
+  if (e.key == 'a') {
+    if (!press.left) {
+      player.spacing = 8
+      press.left = setInterval(() => {
+        for (let i = 0; i < boundaries.length; i++) {
+          const boundary = boundaries[i]
+          if (
+            rectangularCollision({
+              rectangle1: player,
+              rectangle2: {
+                ...boundary,
+                position: {
+                  x: boundary.position.x + 5,
+                  y: boundary.position.y,
+                },
+              },
+            })
+          ) {
+            player.velocity.x = 0
+            break
+          } else player.velocity.x = -movementSpeed
+        }
+      }, 1)
+    }
+  }
+}
+
+function stop(e) {
+  if (e.key == 'w') {
+    clearInterval(press.up)
+    player.velocity.y = 0
+    press.up = undefined
+  }
+  if (e.key == 's') {
+    clearInterval(press.down)
+    player.velocity.y = 0
+    press.down = undefined
+  }
+  if (e.key == 'a') {
+    clearInterval(press.left)
+    player.velocity.x = 0
+    press.left = undefined
+  }
+  if (e.key == 'd') {
+    clearInterval(press.right)
+    player.velocity.x = 0
+    press.right = undefined
   }
 }
